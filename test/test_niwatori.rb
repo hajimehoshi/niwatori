d9532e52f7ff5530462f090ee2a0034796730ae6
hajimehoshi <hajimehoshi@gmail.com>
2009-02-17 05:06:11 +0900

    refactoring

--- test/test_niwatori.rb
+++ test/test_niwatori.rb
@@ -11,33 +11,55 @@ class TestNiwatori < MiniTest::Unit::TestCase
                   [:go_south],
                   [:go_north],
                   [:go_down],
+                  [:go_up],
+                  [:go_east],
+                  [:go_east],
+                  [:go_up],
                  ]
     digraph = Digraph.new(2, 5, 0, directions)
     e = digraph.edges.each
     edge = e.next
     assert_equal(Vertex[2, 5, 0], edge.initial)
     assert_equal(Vertex[2, 4, 0], edge.terminal)
-    edge = e.next
-    assert_equal(Vertex[2, 4, 0], edge.initial)
-    assert_equal(Vertex[2, 5, 0], edge.terminal)
+    edge2 = e.next
+    assert_equal(edge.terminal, edge2.initial)
+    assert_equal(edge.initial, edge2.terminal)
     edge = e.next
     assert_equal(Vertex[2, 4, 0], edge.initial)
     assert_equal(Vertex[1, 4, 0], edge.terminal)
-    edge = e.next
-    assert_equal(Vertex[1, 4, 0], edge.initial)
-    assert_equal(Vertex[2, 4, 0], edge.terminal)
+    edge2 = e.next
+    assert_equal(edge.terminal, edge2.initial)
+    assert_equal(edge.initial, edge2.terminal)
     edge = e.next
     assert_equal(Vertex[1, 4, 0], edge.initial)
     assert_equal(Vertex[1, 5, 0], edge.terminal)
-    edge = e.next
-    assert_equal(Vertex[1, 5, 0], edge.initial)
-    assert_equal(Vertex[1, 4, 0], edge.terminal)
+    edge2 = e.next
+    assert_equal(edge.terminal, edge2.initial)
+    assert_equal(edge.initial, edge2.terminal)
     edge = e.next
     assert_equal(Vertex[1, 5, 0], edge.initial)
     assert_equal(Vertex[1, 5, -1], edge.terminal)
+    edge2 = e.next
+    assert_equal(edge.terminal, edge2.initial)
+    assert_equal(edge.initial, edge2.terminal)
     edge = e.next
     assert_equal(Vertex[1, 5, -1], edge.initial)
-    assert_equal(Vertex[1, 5, 0], edge.terminal)
+    assert_equal(Vertex[2, 5, -1], edge.terminal)
+    edge2 = e.next
+    assert_equal(edge.terminal, edge2.initial)
+    assert_equal(edge.initial, edge2.terminal)
+    edge = e.next
+    assert_equal(Vertex[2, 5, -1], edge.initial)
+    assert_equal(Vertex[3, 5, -1], edge.terminal)
+    edge2 = e.next
+    assert_equal(edge.terminal, edge2.initial)
+    assert_equal(edge.initial, edge2.terminal)
+    edge = e.next
+    assert_equal(Vertex[3, 5, -1], edge.initial)
+    assert_equal(Vertex[3, 5, 0], edge.terminal)
+    edge2 = e.next
+    assert_equal(edge.terminal, edge2.initial)
+    assert_equal(edge.initial, edge2.terminal)
     assert_raises(StopIteration){ e.next }
   end
 
