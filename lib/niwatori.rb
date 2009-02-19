1d0981b228884163f71c52436868b583ff293ea3
hajimehoshi <hajimehoshi@gmail.com>
2009-02-20 04:01:03 +0900

    rewrote

--- lib/niwatori.rb
+++ lib/niwatori.rb
@@ -1,131 +1,213 @@
 module Niwatori
 
-  class DungeonPath
+  module_function
 
-    Node = Struct.new(:x, :y, :z, :switch)
-
-    class Node
-
-      def move(direction)
-        new_node = self.dup
-        case direction
-        when :north
-          new_node.y -= 1
-        when :west
-          new_node.x -= 1
-        when :east
-          new_node.x += 1
-        when :south
-          new_node.y += 1
-        when :up
-          new_node.z += 1
-        when :down
-          new_node.z -= 1
-        when :switch
-          new_node.switch = 1 - new_node.switch
-        else
-          raise "invalid direction: #{direction}"
-        end
-        new_node
-      end
-
-    end
-
-    def initialize
-      @nodes = [Node[0, 0, 0, 0].freeze]
-    end
-
-    def width
-      @size[0]
-    end
-
-    def height
-      @size[1]
-    end
-
-    def nodes
-      @nodes.each
-    end
-
-    def addable?(direction)
-      new_node = @nodes.last.move(direction)
-      !@nodes.include?(new_node)
-    end
-
-    def add(direction)
-      raise "can't add" unless addable?(direction)
-      new_node = @nodes.last.move(direction)
-      @nodes << new_node.freeze
-    end
-
+  def generate_dungeon()
+    generate_rooms(generate_path)
   end
 
-=begin
-
-  class Dungeon
-
-    attr_reader :rooms
-
-    def initialize(digraph)
-      rooms = {}
-      digraph.vertexes.each.with_index do |vertex, i|
-        key = [vertex.x, vertex.y, vertex.z]
-        doors = []
-        digraph.edges.select {|e| e.initial == vertex}.each do |edge|
-          terminal = edge.terminal
-          case vertex.x - terminal.x
-          when 1
-            doors << :west
-          when -1
-            doors << :east
-          end
-          case vertex.y - terminal.y
-          when 1
-            doors << :north
-          when -1
-            doors << :south
-          end
+  def generate_path
+    loop do
+      path = [[0, 0, 0, 0]]
+      until 50 <= path.size and
+          (last = path.last and
+           path[1..-2].all? {|n| n[0..2] != path.last[0..2] })
+        node = path.last.dup
+        next_nodes = []
+        next_nodes << node.dup.tap {|a| a[0] += 1 }
+        next_nodes << node.dup.tap {|a| a[0] -= 1 }
+        next_nodes << node.dup.tap {|a| a[1] += 1 }
+        next_nodes << node.dup.tap {|a| a[1] -= 1 }
+        next_nodes << node.dup.tap {|a| a[2] += 1 }
+        next_nodes << node.dup.tap {|a| a[2] -= 1 }
+        next_nodes << node.dup.tap {|a| a[3] = 1 - a[3] }
+        retry if next_nodes.all? {|n| path.include?(n) }
+        case i = rand(7)
+        when 0..5
+          node[i/2] += (i % 2) * 2 - 1
+        when 6
+          node[3] = 1 - node[3]
         end
-        rooms[key] = Room.new(i == 0, doors)
+        unless path.include?(node)
+          path.push(node)
+        end
       end
-      @rooms = Rooms.new(rooms)
+      return path
     end
+  end
 
+  def generate_rooms(path)
+    connections = {}
+    size = {
+      :max_x => -1.0/0.0,
+      :min_x => 1.0/0.0,
+      :max_y => -1.0/0.0,
+      :min_y => 1.0/0.0,
+      :max_z => -1.0/0.0,
+      :min_z => 1.0/0.0,
+    }
+    path.each_with_index do |node, i|
+      locate = node[0..2]
+      connections[locate] ||= {}
+      if locate[0] < size[:min_x]
+        size[:min_x] = locate[0]
+      elsif size[:max_x] < locate[0]
+        size[:max_x] = locate[0]
+      end
+      if locate[1] < size[:min_y]
+        size[:min_y] = locate[1]
+      elsif size[:max_y] < locate[1]
+        size[:max_y] = locate[1]
+      end
+      if locate[2] < size[:min_z]
+        size[:min_z] = locate[2]
+      elsif size[:max_z] < locate[2]
+        size[:max_z] = locate[2]
+      end
+      neighbor_nodes = []
+      neighbor_nodes << path[i-1] if 0 < i-1
+      neighbor_nodes << path[i+1] if path[i+1]
+      cs = []
+      neighbor_nodes.each do |n|
+        if node[0] - 1 == n[0]
+          cs << :west
+        elsif node[0] + 1 == n[0]
+          cs << :east
+        elsif node[1] - 1 == n[1]
+          cs << :north
+        elsif node[1] + 1 == n[1]
+          cs << :south
+        elsif node[2] - 1 == n[2]
+          cs << :down
+        elsif node[2] + 1 == n[2]
+          cs << :up
+        elsif node[3] != n[3]
+          connections[locate][:switch] = true
+        end
+      end
+      connections[locate][node[3]] = cs if 0 < cs.size
+    end
+    rooms = Rooms.new(connections, size, [0, 0, 0], path.last[0..2])
   end
 
   class Rooms
-    
-    def initialize(rooms)
-      @rooms = rooms
-    end
 
-    def [](x, y, z)
-      @rooms[[x, y, z]]
-    end
-
-    include Enumerable
-
-    def each
-      @rooms.values.each
-    end
-
-  end
-
-  class Room
-
-    attr_reader :doors
-
-    def initialize(start, doors)
+    def initialize(connections, size, start, goal)
+      @connections = connections
+      @size = size
       @start = start
-      @doors = doors
+      @goal = goal
     end
 
-    def start?
-      @start
-    end
+=begin
 
-  end
++-^-----+    +-----v-+  
+| r     |    |       |
+|   K  B|----|   *   |
+|       |    |S      |
++-------+    +-------+
+    ^
+    |
++-------+
+|       |
+|       |
+|G      |
++-------+
+
+S: Starting position
+G: Goal posiion
+
+*: Crystal switch
+r: Red block
+B: Blue block
+
+k: Small key
+K: Big key
+u: Upper stair
+d: Downer stair
+
+x: Small key door
+X: Big key door
 
 =end
+    def to_aa
+      lines = []
+      (@size[:min_z]..@size[:max_z]).reverse_each do |z|
+        lines << ((0 <= z) ? "#{z+1}F" : "B#{-z}F")
+        (@size[:min_y]..@size[:max_y]).each do |y|
+          7.times { lines << "" }
+          (@size[:min_x]..@size[:max_x]).each do |x|
+            if room = @connections[[x, y, z]]
+              new_lines = ["             ",
+                           "  +-------+  ",
+                           "  |       |  ",
+                           "  |       |  ",
+                           "  |       |  ",
+                           "  +-------+  ",
+                           "             "]
+              if [x, y, z] == @start
+                new_lines[4][3] = "S"
+              end
+              if [x, y, z] == @goal
+                new_lines[4][3] = "G"
+              end
+              if room[:switch]
+                new_lines[3][6] = "*"
+              end
+              ((room[0] || []) | (room[1] || [])).each do |door|
+                case door
+                when :north
+                  new_lines[0][6] = "|"
+                when :south
+                  new_lines[6][6] = "|"
+                when :west
+                  new_lines[3][0..1] = "--"
+                when :east
+                  new_lines[3][11..12] = "--"
+                when :up
+                  new_lines[1][z%2 == 0 ? 4 : 8] = "^"
+                when :down
+                  new_lines[1][z%2 == 0 ? 8 : 4] = "v"
+                end
+              end
+              if room[0] and room[1]
+                [["r", 0, 1], ["B", 1, 0]].each do |color, i1, i2|
+                  (room[i1] - room[i2]).each do |door|
+                    case door
+                    when :north
+                      new_lines[2][6] = color
+                    when :south
+                      new_lines[4][6] = color
+                    when :west
+                      new_lines[3][3] = color
+                    when :east
+                      new_lines[3][9] = color
+                    when :up
+                      new_lines[2][z%2 == 0 ? 4 : 8] = color
+                    when :down
+                      new_lines[2][z%2 == 0 ? 8 : 4] = color
+                    end
+                  end
+                end
+              end
+              new_lines.each_with_index do |line, i|
+                lines[i - new_lines.size] << line
+              end
+            else
+              (-7..-1).each {|i| lines[i] << "             " }
+            end
+          end
+        end
+      end
+      lines.join("\n")
+    end
+
+  end
+
+end
+
+if $0 == __FILE__
+
+  puts Niwatori.generate_dungeon.to_aa
 
 end
